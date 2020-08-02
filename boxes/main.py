1f5b2f2efd4c4b87cd2e40d3e31e7da778a61f70
takenakaroberta <takenaka.roberta@gmail.com>
2020-08-02 15:00:16 -0300

    Ajustes para rodar no Windows

--- boxes/main.py
+++ boxes/main.py
@@ -1,14 +1,13 @@
 import os
 import csv
 import argparse
-
+from datetime import datetime
 from PIL import Image
 
 from utils.files import FileInfo
 
 
 def get_files(path):
-    print(path)
     if os.path.isfile(path):
         return [path]
 
@@ -16,7 +15,6 @@ def get_files(path):
     files = []
     for path in os.listdir(dirname):
         fullpath = os.path.join(dirname, path)
-        print(fullpath)
         if os.path.isfile(fullpath):
             files.append(fullpath)
         elif os.path.isdir(fullpath):
@@ -27,25 +25,67 @@ def get_files(path):
 def classify_files(files):
     for file_path in files:
         fileinfo = FileInfo(file_path)
+        data = {"type": ""}
         try:
-            Image(file_path)
-        except TypeError:
-            data = {"type": ""}
+            Image.open(file_path)
+        except Exception as e:
+            ign, ext = os.path.splitext(file_path)
+            if ext.startswith(".mp"):
+                data = {"type": "video"}
         else:
             data = {"type": "image"}
-        data.update(fileinfo.data)
+        
+        data.update(get_data(fileinfo))
         yield data
 
 
-def write(csv_file_path, files):
-    with open(csv_file_path, 'w', newline='') as csvfile:
-        fieldnames = ['type', 'hash', 'size', "birth_time", "name", "file_path", "dateiso", "time"]
+def isoformat(time):
+    return datetime.fromtimestamp(time).isoformat()[:10]
 
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for data in files:
-            print(data)
-            writer.writerow(data)
+
+def get_data(fileinfo):
+    return {
+        "file_path": fileinfo.file_path,
+        "size": fileinfo.size,
+        "first_time": fileinfo.first_time,
+        "first_date": isoformat(fileinfo.first_time),
+        "birth_date": isoformat(fileinfo.birth_time),
+        "c_date": isoformat(fileinfo.c_time),
+        "recent_access_date": isoformat(fileinfo.recent_access_time),
+        "modification_date": isoformat(fileinfo.modification_time),
+        "hash": fileinfo.hash,
+        "name": fileinfo.basename,
+    }
+
+
+def write(csv_file_path, files):
+    fieldnames = ['type', 'hash', 'size', "first_time", 
+                  "first_date",
+                  "birth_date",
+                  "c_date",
+                  "recent_access_date",
+                  "modification_date",
+                  "name", "file_path",
+                  ]
+    img_csv_file_path = csv_file_path.replace(".csv", ".img.csv")
+    vid_csv_file_path = csv_file_path.replace(".csv", ".vid.csv")
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        with open(vid_csv_file_path, 'w', newline='') as vid_csvfile:
+            with open(img_csv_file_path, 'w', newline='') as img_csvfile:
+                vid_writer = csv.DictWriter(vid_csvfile, fieldnames=fieldnames)
+                img_writer = csv.DictWriter(img_csvfile, fieldnames=fieldnames)
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                vid_writer.writeheader()
+                img_writer.writeheader()
+                writer.writeheader()
+                for data in files:
+                    print(data.get("file_path"))
+                    if data.get("type") == "image":
+                        img_writer.writerow(data)
+                    elif data.get("type") == "video":
+                        vid_writer.writerow(data)
+                    else:
+                        writer.writerow(data)
 
 
 def main():
--- boxes/utils/files.py
+++ boxes/utils/files.py
@@ -26,28 +26,39 @@ class FileInfo:
         return self._statinfo.st_size
 
     @property
+    def first_time(self):
+        return sorted([self.birth_time, self.recent_access_time, self.modification_time, self.c_time])[0] 
+
+    @property
     def birth_time(self):
-        return self._statinfo.st_birthtime
+        try:
+            return self._statinfo.st_birthtime
+        except AttributeError:
+            return self._statinfo.st_ctime
+
+    @property
+    def recent_access_time(self):
+        return self._statinfo.st_atime
+
+    @property
+    def modification_time(self):
+        return self._statinfo.st_mtime
+
+    @property
+    def c_time(self):
+        return self._statinfo.st_ctime
 
     @property
     def content(self):
-        with open(self._file_path, 'rb') as fp:
-            return fp.read()
+        try:
+            with open(self._file_path, 'rb') as fp:
+                c = fp.read()
+        except Exception as e:
+            return b''
+        else:
+            return c
 
     @property
     def hash(self):
         m = hashlib.sha256(self.content)
         return m.digest()
-
-    @property
-    def data(self):
-        d = datetime.fromtimestamp(self.birth_time).isoformat()
-        return {
-            "file_path": self._file_path,
-            "size": self.size,
-            "birth_time": self.birth_time,
-            "hash": self.hash,
-            "name": self.basename,
-            'dateiso': d[:10],
-            'time': d[11:],
-        }
