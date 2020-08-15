2c1c43ef83252ed65f6ef75761f2c2617740df07
takenakaroberta <takenaka.roberta@gmail.com>
2020-08-14 22:03:09 -0300

    Cria um script para cópia de pasta, mas em uma nova estrutura

---
+++ boxes/create_copy_script.py
@@ -0,0 +1,54 @@
+import os
+import csv
+import argparse
+import logging
+import shutil
+
+"""
+def organize(file_path, dest_path):
+    dest_path = os.path.normpath(dest_path)
+    if not os.path.isdir(dest_path):
+        os.makedirs(dest_path)
+    with open(file_path, 'r') as fp:
+        for item in fp.read().splitlines():
+            if "\t" in item:
+                src, dst = item.split("\t")
+                dst_file_path = os.path.join(dest_path, dst)
+                dirname = os.path.dirname(dst_file_path)
+                if not os.path.isdir(dirname):
+                    os.makedirs(dirname)
+                shutil.copyfile(src, dst_file_path)
+"""
+
+def create_script_to_copy(file_path, dest_path):
+    dest_path = os.path.normpath(dest_path)
+    mkdir = []
+    copy = []
+    with open(file_path, 'r') as fp:
+        for item in fp.read().splitlines():
+            if "\t" in item:
+                src, dst = item.split("\t")
+                dst_file_path = os.path.join(dest_path, dst)
+                dirname = os.path.dirname(dst_file_path)
+                if not os.path.isdir(dirname):
+                    mkdir.append('mkdir -p "{}"'.format(dirname))
+                if not os.path.isfile(dst_file_path):
+                    copy.append('cp "{}" "{}"'.format(src, dst_file_path))
+    with open(file_path + ".sh", "w") as fp:
+        fp.write("\n".join(sorted(list(set(mkdir)))) + "\n" + "\n".join(sorted(copy)))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        'Dada uma lista de fonte e destino, e uma pasta raíz do destino,'
+        ' cria um script para '
+        'fazer cópia em uma nova estrutura')
+    parser.add_argument("txtfile", help="text file")
+    parser.add_argument("destination", help="destination path")
+    args = parser.parse_args()
+
+    create_script_to_copy(args.txtfile, args.destination)
+    
+
+if __name__ == '__main__':
+    main()
